5688048c20fd84a0775d25713b9f18048d6d3087
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-03-17 12:32:57 -0400

    changed variable name to signify a jQuery variable

--- jQuery_basics_js_file.js
+++ jQuery_basics_js_file.js
@@ -1,2 +1,5 @@
-var headings = jQuery(':header');
-console.log("Number of elements matched:", headings.length);
+var $headings = jQuery(':header'); // select and store all h1-h6 elements
+// OR USE $(':header') to select all h1-h6 elems - shortened version of selector
+console.log("\nNumber of elements matched:", headings.length); // output the # of results
+//TODO: create and display new element to show output
+//TODO: turn all headings selected to color: red
