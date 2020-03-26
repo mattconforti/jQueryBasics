de4f381e6d0fe234b768d86eeb6a3dd747f7cf18
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-03-26 15:33:25 -0400

    added jQuery script and 2 jQuery selections

---
+++ jQuery_basics_2/jQuery_basics_2_script.js
@@ -0,0 +1,9 @@
+// store the checkbox and text elements in jQuery objects
+var $checkboxInputs = $('input[type="checkbox"]');
+var $textInputs = $('input[type="text"]');
+
+// output to console for debugging
+console.log($checkboxInputs);
+console.log($textInputs);
+
+
