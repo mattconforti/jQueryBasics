728b8ff04381094f279ce489c2db35b64749696e
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-04-09 11:17:40 -0400

    style refactoring

--- jQuery_basics_2/jQuery_basics_2_script.js
+++ jQuery_basics_2/jQuery_basics_2_script.js
@@ -1,24 +1,22 @@
 $('#add_button').on('click', function() {
-
     // store list items in jQuery variable
     var $listItems = $('.list_item');
 
     // create counter for how many list items we have
     var counter = $listItems.length;
-
-    // add to the counter as we are about to create a new item
     counter++;
 
-    // create the new list item
+    // create the new list item and insert it
     const $newItem = $(`<div class="list_item">
-    <input type="checkbox" id="check${counter}" name="item${counter}" value="TODO${counter}">
-    <input type="text" id="in${counter}" name="input${counter}" placeholder="Enter text here">
+    <input type="checkbox" id="check${counter}" name="item${counter}" 
+     value="TODO${counter}">
+    <input type="text" id="in${counter}" name="input${counter}" 
+     placeholder="Enter text here">
     </div>`);
 
-    // insert new item after the current last item
     $listItems.last().after($newItem);
 
-    // get current set of text inputs for debugging
+    // get current set of inputs for debugging
     var $textInputs = $('input[type="text"]');
     $textInputs.each(function() {
         console.log("Input ID# " + this.id);
@@ -26,6 +24,9 @@ $('#add_button').on('click', function() {
 });
 
 $('#clear_button').on('click', function() {
-    $('input[type="text"]').val('');  // .val() method to set value of input field
-    $('input[type="checkbox"]').prop("checked", false);  // .prop() method to uncheck boxes
+    // use .val() to set value of inputs
+    $('input[type="text"]').val('');
+
+    // use .prop() to uncheck boxes
+    $('input[type="checkbox"]').prop("checked", false);
 });
