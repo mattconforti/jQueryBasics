3f3de1b40f242ea03c2ca8fe81192a62065bca56
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-04-14 14:24:30 -0400

    created paragraphs and show/hide toggle functionality

--- jQuery_basics_3/jQuery_basics_3_script.js
+++ jQuery_basics_3/jQuery_basics_3_script.js
@@ -1,3 +1,21 @@
-$('#learn_button').on('click', function() {
+// create variables for all elements in use
+var $paragraphs_not_one = $('p:not(#p_one)');
+var $toggle_button = $('#show_button');
 
+// hide all paragraphs without id: p_one
+$paragraphs_not_one.hide();
+
+// on button click,
+$toggle_button.on('click', function() {
+
+    // toggle btw show/hide the other paragraphs
+    $paragraphs_not_one.toggle();
+
+    // change the button text depending on visibility
+    if ($paragraphs_not_one.is(":visible")) {
+        $toggle_button.text("Show Less");
+    }
+    else {
+        $toggle_button.text("Show More");
+    }
 });
