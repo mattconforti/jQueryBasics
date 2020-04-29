085babc8cc8a2cdbd0c85cc1695a4f8ab64db036
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-04-29 14:02:48 -0400

    changed card flip animation to occur upon button click

--- jQuery_basics_4/jQuery_basics_4_script.js
+++ jQuery_basics_4/jQuery_basics_4_script.js
@@ -1,20 +1,25 @@
 $(function() {
     var isFlipped = false;
     var $flip_button = $('#flip_button');
-    var $flip_card = $('flip_card');
+    var $flip_card = $('.flip_card');
+    var $flip_card_inner = $('.flip_card_inner');
 
     $flip_button.on('click', function() {
+        // output value of isFlipped for debugging
+        console.log(isFlipped);
+
         if (!isFlipped) {
             console.log('not flipped');
-            //TODO: animate flip
-            //      set isFlipped to 'true'
-            //      exit
+            /*
+            $flip_card.css('box-shadow', 'none');
+            */
+            $flip_card_inner.css('transform', 'rotateY(180deg)');
+            isFlipped = true;  // now the card is flipped to back
         }
         else {
             console.log('flipped');
-            //TODO: animate flip back to front side
-            //      set isFlipped back to 'false'
-            //      exit
+            $flip_card_inner.css('transform', 'initial');
+            isFlipped = false;  // card is on front side now
         }
     });
 });
