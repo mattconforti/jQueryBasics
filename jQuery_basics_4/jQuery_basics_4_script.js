c6b93d6ab4ddbacb9c9a18c69d79793cce1a1104
Matt Conforti <mattconforti@matts-Mac-mini.local>
2020-04-30 13:13:33 -0400

    fixed box shadow bug on flip animation

--- jQuery_basics_4/jQuery_basics_4_script.js
+++ jQuery_basics_4/jQuery_basics_4_script.js
@@ -8,11 +8,11 @@ $(function() {
         // output value of isFlipped for debugging
         console.log(isFlipped);
 
+        // rid of box shadow in prep for flip
+        $flip_card.css('box-shadow', 'none');
+
         if (!isFlipped) {
             console.log('not flipped');
-            /*
-            $flip_card.css('box-shadow', 'none');
-            */
             $flip_card_inner.css('transform', 'rotateY(180deg)');
             isFlipped = true;  // now the card is flipped to back
         }
@@ -21,5 +21,10 @@ $(function() {
             $flip_card_inner.css('transform', 'initial');
             isFlipped = false;  // card is on front side now
         }
+
+        // put box shadow back on after flip animation (1500ms animation)
+        setTimeout(function(){
+            $flip_card.css('box-shadow', '4px 4px 8px 10px rgba(0,0,0,0.2)');
+        }, 1500);
     });
 });
